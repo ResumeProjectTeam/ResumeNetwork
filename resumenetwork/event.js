3a749f9c41314deaa1d22c8aef07333b2761d02d
Ubuntu <ubuntu@ip-172-31-24-27.ap-northeast-2.compute.internal>
2018-05-18 13:39:37 +0000

    event.js 수정2

--- resumenetwork/event.js
+++ resumenetwork/event.js
@@ -138,7 +138,6 @@ class SitechainListener{
         		     
                             createAuthentication.userId =  addRequestUser.requestUserId;
 
-        		    console.log(createAuthentication);
 			
 		            this.NetworkConnection.submitTransaction(createAuthentication);
              
